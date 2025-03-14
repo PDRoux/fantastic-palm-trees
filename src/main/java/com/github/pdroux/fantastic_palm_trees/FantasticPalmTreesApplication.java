97add860c5eeaf31fba14091bb23c9ac44f97a0c
PD Roux <PaulDermottRoux@gmail.com>
2025-03-14 11:59:04 +0200

    Initial spring boots - maven setup

---
+++ src/main/java/com/github/pdroux/fantastic_palm_trees/FantasticPalmTreesApplication.java
@@ -0,0 +1,12 @@
+package com.github.pdroux.fantastic_palm_trees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasticPalmTreesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasticPalmTreesApplication.class, args);
+	}
+}
